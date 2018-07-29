29cb2679e5f67c41cce978d1c0b98b4c1e8893a8
yc322 <33708548+yc322@users.noreply.github.com>
2018-07-29 20:57:32 +0800

    Create Buffer.cpp

---
+++ Buffer.cpp
@@ -0,0 +1,107 @@
+#include "Buffer.h"
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+/**********************************************************
+
+			Buffer
+
+***********************************************************/
+
+Buffer::Buffer(unsigned long size)
+{
+	capacity_ = size;
+	head_ = new char[size];
+	free_=head_;
+}
+
+Buffer::Buffer(void* head,unsigned int size,void*free)
+{
+	head_=head;
+	capacity_=size;
+	free_=free;
+	if(free==NULL)
+		this->free_=reinterpret_cast<char*>(head)+size;
+}
+
+
+Buffer::~Buffer()
+{
+	delete[] reinterpret_cast<char*>(head_);
+	head_=0;
+	free_=NULL;
+	capacity_=0;
+}
+
+
+bool Buffer::is_valid_add(void* loc ,unsigned int length)
+{
+	return (head_ <= loc) && (loc <= ((char*)head_+capacity_ -length));
+}
+
+bool Buffer::can_store(unsigned int size)
+{
+	return is_valid_add(free_,size);
+}
+
+void* Buffer::allocate(unsigned int len)
+{
+	if(!can_store(len)) return NULL;
+	void* ret=free_;
+	free_ = reinterpret_cast<char*>(free_)+len;
+	return ret;
+}
+
+const unsigned int Buffer::used_space()
+{
+	return reinterpret_cast<char*>(free_)-reinterpret_cast<char*>(head_);
+}
+
+const unsigned int Buffer::cur_capacity()
+{
+	return capacity_;
+}
+
+
+/*************************************************************
+
+					TupleBuffer
+
+*************************************************************/
+
+TupleBuffer::TupleBuffer(unsigned long size , unsigned int tuplesize)
+	:Buffer(size),tuplesize_(tuplesize)
+{
+	if(size < tuplesize){
+		cout<<"size must be greater than tuplesize!"<<endl;
+	}
+}
+
+TupleBuffer::TupleBuffer(void* data,unsigned int size, void* free,unsigned int tuplesize)
+	:Buffer(data,size,free),tuplesize_(tuplesize)
+{
+	if(size < tuplesize)
+		cout<<"size must be greater than tuplesize!"<<endl;
+}
+
+void* TupleBuffer::get_tuple_offset(unsigned int pos)
+{
+	char* f = reinterpret_cast<char*>(free_);
+	char* d = reinterpret_cast<char*>(head_);
+	char* ret = d + static_cast<unsigned long long>(pos)*tuplesize_;
+	return ret < f ? ret : 0;
+}
+
+
+bool TupleBuffer::is_valid_tuple_add(void* loc)
+{
+	return Buffer::is_valid_add(loc,tuplesize_);
+}
+
+
+void* TupleBuffer::allocate_tuple()
+{
+	return Buffer::allocate(tuplesize_);
+}
