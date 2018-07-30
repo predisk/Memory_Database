d4adf65897d75ae76970ce25aa33c4ad730f6748
PudgeXD <609857240@qq.com>
2018-07-30 20:53:28 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
-#include"page.h"
+#include<fstream>
+#include<assert.h>
 #include"schema.h"
-#include"table.h"
+//#include"table.h"
 
 using namespace std;
 
+//vector<WriteTable*>openTables;
+vector<string>existTables;
+
+void init();
+//WriteTable* isOpen(string tableName);
+bool isExist(string tableName);
+//WriteTable loadTable(string tableName);
+
 int main()
 {
+    init();
     while(1)
     {
 
         string input;
         cout << "please input: ";
 
-        cin >> input;
-        //input = "CREATE TABLE Taxi (id CHAR(10),x INT,y INT);" ;
+        getline(cin,input);
+
+        //cout << input.compare("CREATE TABLE Taxi (id CHAR(10),x INT,y INT);");
 
         string head = input.substr(0,input.find(' '));
         string remain = input.substr(input.find(' ')+1,input.length());
 
         if(!head.compare("CREATE"))
         {
+            //"CREATE TABLE Taxi (id CHAR(10),x INT,y INT);"
             string second = remain.substr(0,remain.find(' ')); //TABLE
             remain = remain.substr(remain.find(' ')+1,remain.length());
 
             Schema s;
             s.create(remain);
-
             // create a table using s
+            //openTables.push_back(table)`
 
         }
         else if(!head.compare("LOAD"))
@@ -62,4 +74,41 @@ int main()
             cout << "Invalid command" <<endl;
         }
    }
+}
+
+WriteTable* isOpen(string tableName)
+//test
+{
+    for(int i=0;i<openTables.size();i++)
+    {
+        if(!((openTables[i]->schema_).tableName_).compare(tableName))
+            return openTables[i];
+    }
+    return NULL;
+}
+
+bool isExist(string tableName)
+//test
+{
+    string tmp;
+    for(int i=0;i<existTables.size();i++)
+    {
+        tmp = existTables[i].tableName_;
+        if(!tmp.compare(tableName))
+            return true;
+    }
+    return false;
+}
+
+void init()
+{
+    ifstream metadata;
+    metadata.open("medata.txt");
+    assert(metadata.is_open());
+
+    string line;
+    while(getline(metadata,line))
+    {
+        existTables.push_back(line);
+    }
 }
