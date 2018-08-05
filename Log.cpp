9ae418d29bf06ac99e5f079bf81c1437144303f0
predisk <38853435+predisk@users.noreply.github.com>
2018-08-05 20:04:27 +0800

    Update Log.cpp
    The functions including check point and auto flush log are finished, and the recovery is waiting for the parse of the main. Now it can read log from the disk from check point to the end and parse it to char*, recovering it to the original command, and the rest part just need to do the same as what parse of the main do.

--- Log.cpp
+++ Log.cpp
@@ -11,66 +11,56 @@
 
 
 #define BUFFERSIZE 1024
-#define TIMER 1000*60//定时一分钟
+#define TIMER 1000//定时
 using namespace std;
 const char* LogPath="D:\\sample.dat";
 const char* LogPath1="D:\\sample1.txt";
 //初始化
 Log::Log(){
-    //std::ofstream ofs(LogPath, ofstream::binary|ofstream::app);
-    //ofs.close();
-    //cout << "Success open files" << endl;
-    //Sleep(TIMER);
-    flag = 1;
+    flag = 0;
     get_buffer();
-    _beginthread(Timer, 0, this);
+    //_beginthread(Timer, 0, this);
     //NULL, 0, &SecondThreadFunc, NULL, 0, &threadID
     //hThread = (HANDLE)_beginthreadex(NULL, 0, &Timer,this,0,&threadID);
-
-
  }
 Log::~Log(){
-    flag = 0;
+    //while(ifInputFile.peek() != EOF)
     //CloseHandle( hThread );
-    write_in_disk();
-     //cout << "content: " <<endl;
-     //cout<< bufferdata_ << endl;
+    if(use_lenth != 0){
+        write_in_disk();
+    }
     delete[] bufferdata_;
-    cout << "1" <<endl;
-
 }
 
-void Log::Timer(void *p)
-{
+void Log::Timer(void *p) {
     int counter = 0;
     Log *handle = (Log *)p;
 
     /**自动刷新*/
     while(1){
-        handle->write_in_time();
         Sleep(TIMER);
+        handle->auto_write();
     }
-
 }
-Log::write_in_time(){
-    cout << "in time" << endl;
-    write_in_disk();
+
+Log::auto_write() {
+    //cout << "in time" << endl;
+    if(use_lenth != 0){
+        write_in_disk();
+    }
 }
 
 Log::get_buffer() {
     //char *bufferdata_;
-    //char *end_;
     bufferdata_= new char[BUFFERSIZE];
     end_ = bufferdata_;
     start_ = bufferdata_;
     //cout << "success" << endl;
     use_lenth = 0;
-    //use_lenth = reinterpret_cast<int>(end_ - start_);
-    //cout<< use_lenth << endl;
     return 0;
 }
 
-Log::write(char *command){
+Log::write(char *command) {
     int data_lenth;
     if((data_lenth = strlen(command)) != 0){
         write_in_buffer(command,data_lenth);
@@ -81,58 +71,109 @@ Log::write(char *command){
 
 Log::write_in_buffer(char *command, int data_lenth){
 
-    use_lenth = reinterpret_cast<int>(end_) - reinterpret_cast<int>(start_) ;
+    //use_lenth = reinterpret_cast<int>(end_) - reinterpret_cast<int>(start_) ;
     char endl_ = '\n';
-    if((data_lenth + use_lenth) > BUFFERSIZE){
+    if((data_lenth + use_lenth + 25) > BUFFERSIZE){
         write_in_disk();
     }
     get_time();
     if(use_lenth == 0){
         strcpy(bufferdata_,time);
+    }
+    else{
+        strncat(bufferdata_,time,26);
+    }
         strncat(bufferdata_,command,data_lenth);
         strcat(bufferdata_,"\n");
 
-    }
-    else{
-        //memset(bufferdata_, '0' , 1);
-        strncat(bufferdata_,time,26);
-        strncat(bufferdata_,command,data_lenth);
-        strcat(bufferdata_,"\n");
-    }
     //cout << "content: " << bufferdata_ << endl;
     //cout << "test" <<endl;
     end_ += data_lenth + 25;
     //cout << end_ << endl;
     use_lenth = reinterpret_cast<int>(end_) - reinterpret_cast<int>(start_) ;
+    flag = 1;
     cout << "use lenth :" << use_lenth << endl;
     return 0;
 
 }
 
-Log::write_in_disk(){
+Log::write_in_disk() {
     //cout << "content: " << bufferdata_ << endl;
-    //cout << use_lenth << endl;
     std::ofstream ofs(LogPath, ofstream::binary|ofstream::app);
     ofs.write(bufferdata_,use_lenth);
     ofs.close();
-    //memset(bufferdata_, '0' , lenth);
-    //end_ = start_;
-    FILE* fp=NULL;
-    fp = fopen(LogPath1, "a+");
-    fwrite(bufferdata_ , 1, use_lenth , fp );
-    fclose (fp);
     delete[] bufferdata_;
     get_buffer();
-
-
 }
-Log::flush_log(){
-    write_in_disk();
+
+
+
+Log::flush_log() {
+    if(use_lenth!=0){
+        write_in_disk();
+        checkpoint();
+        flag = 0;
+    }
+    else if(flag != 0){
+        /**更新检查点*/
+        checkpoint();
+    }
+}
+
+Log::checkpoint() {
+    int n;
+    std::ifstream in(LogPath, ifstream::binary);
+    //in.seekg (0, ifstream::beg);
+    //a = in.tellg();
+    in.seekg (0, ifstream::end);
+    n = in.tellg();
+    in.close();
+    //cout << "n = " << n << endl;
+    std::ofstream out(LogPath1, ofstream::trunc|ofstream::out);
+    if(out.is_open()){
+        out << n;
+        out.close();
+    }
+
+
 }
 
 Log::get_time() {
-  SYSTEMTIME st;
-  ::GetLocalTime(&st);
-  sprintf(time, "%04d-%02d-%02d %02d:%02d:%02d.%d ", st.wYear, st.wMonth, st.wDay, st.wHour,st.wMinute, st.wSecond, st.wMilliseconds);
-  return 0;
+    SYSTEMTIME st;
+    ::GetLocalTime(&st);
+    sprintf(time, "%04d-%02d-%02d %02d:%02d:%02d.%d ", st.wYear, st.wMonth, st.wDay, st.wHour,st.wMinute, st.wSecond, st.wMilliseconds);
+    return 0;
 }
+
+Log::recover(){
+/**判断是否成功打开 没有成功打开则pos = 0*/
+    int pos;
+    std::ifstream ifs(LogPath1,ifstream::out);
+    if(ifs.is_open()){
+        ifs>>pos;
+    }
+    else{
+        pos = 0;
+    }
+    cout << pos << endl;
+
+    ifs.close();
+
+    int len = 0;
+    while(getline(ifs1,s)){
+        cout<<"content = "<< s<<endl;
+        char *data;
+        s1 = s.substr(24);
+        
+        len = s1.length();
+        data = (char *)malloc((len+1)*sizeof(char));
+        s1.copy(data,len,0);
+        cout<<"data = " << data<<endl;;
+        delete[] data;
+    }
+
+
+    ifs1.close();
+
+}
+
