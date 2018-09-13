2a8037f77f1ac2c01092d128fc886c6ff54fbf2b
PudgeXD <609857240@qq.com>
2018-09-13 08:22:55 +0800

    Update Manager.h

--- Manager.h
+++ Manager.h
@@ -7,16 +7,18 @@
 class Manager
 {
 private:
-    vector<WriteTable*>openTables_;
-    vector<bool>isModify_;
-    vector<string>existTables_;
+    vector<WriteTable*>openTables_; //the tables opened in the memory
+    vector<bool>isModify_;  // the flags meaning the table is modified or not
+    vector<string>existTables_;  // all table names the db including
     int bufferSize_;
-    string path_;
-    string fileType_;
+    string path_; // the path of all table file/metadata.txt
+    string fileType_; // the file type of all the table file/metadata.txt
 protected:
     //load the table from disk to memory
     WriteTable* loadTable(string tableName); //ok
-
+    
+    //load schema from metadata file
+    //if nonexisted return 0
     Schema* loadSchema(string tableName); //ok
 
     //return the ptr if table is in the memory(openTables)
@@ -37,10 +39,13 @@ public:
     //if table is existed, return the ptr to the table
     WriteTable* getTable(string tableName); //ok
 
+    //save one table into disk but not delete it from memory
     bool preserve(WriteTable* ptr);
 
+    //save all the tables in the openTables_
     int auto_preserve();
 
+    //when created
     bool saveSchema(string schema);
 
     void modify(WriteTable* w);
